22f57683b3f448461b98c09e87d15b688ab23ab0
Tarun-Bagga <tarunbagga141@gmail.com>
2026-07-03 11:05:56 +0530

    Basic exploration of dataset

--- analysis.py
+++ analysis.py
@@ -1,5 +1,15 @@
 import pandas as pd
 df = pd.read_csv("data/netflix_titles.csv")
 
-print("DataSet Loaded--->")
+print("=" * 50)
+print("Netflix Dataset")
+print("=" * 50)
+
+print("\n Dataset shape")
 print(df.shape)
+
+print("\n First 5 rows: ")
+print(df.head(5))
+
+print("\n Column names")
+print(df.columns.tolist())
