25efd86252a0ad721dc83bf7aa5349b15de86f3f
Tarun-Bagga <tarunbagga141@gmail.com>
2026-07-06 12:27:07 +0530

    Completed exploratory data analysis for Netflix dashboard

--- analysis.py
+++ analysis.py
@@ -21,7 +21,7 @@ def clean_data(df):
 
     return clean_df
 
-def plot_content_type(clean_df):
+"""def plot_content_type(clean_df):
     type_counts = clean_df["type"].value_counts()
 
     print(type_counts)
@@ -143,7 +143,7 @@ def plot_ratings(clean_df):
     plt.figure(figsize = (10, 6))
     plt.bar(rating_counts.index, rating_counts.values)
     plt.bar(rating_counts.index, rating_counts.values)
-    plt.title("Rating Distribution")
+    plt.title("Rating Distribution Chart")
 
     plt.xlabel("Rating")
     plt.ylabel("Number of Titles")
@@ -153,12 +153,60 @@ def plot_ratings(clean_df):
     plt.tight_layout()
     plt.savefig("images/charts/rating_distribution.png")
 
+    plt.show()"""
+
+def analyze_duration(clean_df):
+
+    movies = clean_df[clean_df["type"] == "Movie"]
+
+    print("\n First 10 movie durations: ")
+    print(movies['duration'].head(10))
+
+def transform_movies_duration(clean_df):
+
+    movie_df = clean_df[clean_df["type"] == "Movie"].copy()
+
+    movie_df["duration"] = (movie_df["duration"]
+                            .str.replace("min", "", regex = False)
+                            .astype(int))
+
+    print("\nFirst 10 Converted Durations:\n")
+    print(movie_df["duration"].head(10))
+
+    print(movie_df["duration"].dtype)
+    return movie_df
+
+def analyze_movie_duration(movie_df):
+
+    print("\n Movie duration statistics: ")
+
+    print(f"Total movies: {len(movie_df)}")
+    print(f"Shortest movie: {movie_df['duration'].min()} minutes")
+    print(f"Longest movie: {movie_df['duration'].max()} minutes")
+    print(f"Average duration: {movie_df['duration'].mean():.2f} minutes")
+    print(f"Median duration: {movie_df['duration'].median()} minutes")
+
+def plot_movie_duration(movie_df):
+
+    plt.figure(figsize = (10, 6))
+
+    plt.hist(movie_df["duration"], bins = 20)
+
+    plt.title("Distribution of Movie Durations on Netflix")
+    plt.xlabel("Duration (minutes)")
+    plt.ylabel("Number of Movies")
+
+    plt.grid(axis = "y", linestyle = "--", alpha = 0.6)
+
+    plt.tight_layout()
+    plt.savefig("images/charts/movie_duration_distribution.png")
+
     plt.show()
 
 def main():
     df = load_data()
     clean_df = clean_data(df)
-    plot_content_type(clean_df)
+    """plot_content_type(clean_df)
 
     analyze_release_year(clean_df)
     plot_release_year(clean_df)
@@ -173,7 +221,12 @@ def main():
     plot_top_genres(genre_df)
 
     analyze_ratings(clean_df)
-    plot_ratings(clean_df)
+    plot_ratings(clean_df)"""
+
+    analyze_duration(clean_df)
+    movie_df = transform_movies_duration(clean_df)
+    analyze_movie_duration(movie_df)
+    plot_movie_duration(movie_df)
 
 if __name__ == "__main__":
     main()
