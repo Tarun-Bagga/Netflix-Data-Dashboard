39c466a56c0ff90db5abcda2c09cd0d969b92093
Tarun-Bagga <tarunbagga141@gmail.com>
2026-07-07 12:53:07 +0530

    Added interactive content type filter to Streamlit dashboard

--- app/app.py
+++ app/app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import pandas as pd
 
+
+st.set_page_config(
+    page_title="Netflix Data Dashboard",
+    page_icon="🎬",
+    layout="wide"
+)
+
+
 @st.cache_data
 def load_data():
-
     df = pd.read_csv("data/netflix_titles.csv")
     return df
 
+
 @st.cache_data
 def clean_data(df):
-
     clean_df = df.copy()
 
     clean_df["director"] = clean_df["director"].fillna("Unknown")
@@ -21,37 +28,61 @@ def clean_data(df):
 
     return clean_df
 
-st.set_page_config(page_title = "Netflix Data Dashboard",
-                   page_icon = "🎬",
-                   layout = "wide")
+
+df = load_data()
+clean_df = clean_data(df)
 
 
 st.title("🎬 Netflix Data Dashboard")
 st.markdown("Analyze Netflix movies and TV shows using interactive visualizations.")
 
-df = load_data()
-clean_df = clean_data(df)
 
-total_titles = len(clean_df)
-movies = len(clean_df[clean_df['type'] == 'Movie'])
-tv_shows = len(clean_df[clean_df['type'] == 'TV Show'])
-countries = clean_df['country'].dropna().str.split(",").explode().str.strip().nunique()
+st.sidebar.header("Filters")
+
+content_type = st.sidebar.selectbox(
+    "Select Content Type",
+    ["All", "Movie", "TV Show"]   # <-- FIXED
+)
+
+
+filtered_df = clean_df.copy()
+
+if content_type != "All":
+    filtered_df = filtered_df[filtered_df["type"] == content_type]
+
+
+total_titles = len(filtered_df)
+
+movies = len(filtered_df[filtered_df["type"] == "Movie"])
+
+tv_shows = len(filtered_df[filtered_df["type"] == "TV Show"])
+
+countries = (
+    filtered_df["country"]
+    .dropna()
+    .str.split(",")
+    .explode()
+    .str.strip()
+    .nunique()
+)
+
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     st.metric("Total Titles", total_titles)
+
 with col2:
     st.metric("Movies", movies)
+
 with col3:
     st.metric("TV Shows", tv_shows)
+
 with col4:
     st.metric("Countries", countries)
+
 
 st.divider()
 
 st.subheader("Dataset Preview")
-st.dataframe(df)
-
-
-st.dataframe(df)
+st.dataframe(filtered_df)
