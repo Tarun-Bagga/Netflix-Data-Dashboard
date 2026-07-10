83fd5ad44e9fdf9e9988126fe240caa0eefea872
Tarun-Bagga <tarunbagga141@gmail.com>
2026-07-10 13:34:36 +0530

    Complete Netflix Data Dashboard v1.0

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from scipy.conftest import xp_available_backends
 
 st.set_page_config(
     page_title="Netflix Data Dashboard",
@@ -9,11 +8,9 @@ st.set_page_config(
     layout="wide"
 )
 
-
 @st.cache_data
 def load_data():
     return pd.read_csv("data/netflix_titles.csv")
-
 
 @st.cache_data
 def clean_data(df):
@@ -28,39 +25,28 @@ def clean_data(df):
 
     return clean_df
 
-
-# -----------------------------
 # Load Data
-# -----------------------------
 df = load_data()
 clean_df = clean_data(df)
 
-
-# -----------------------------
 # Title
-# -----------------------------
 st.title("🎬 Netflix Data Dashboard")
 st.markdown("Interactive dashboard for exploring Netflix movies and TV shows.")
 
-
-# -----------------------------
 # Sidebar Filters
-# -----------------------------
 st.sidebar.title("🎛 Dashboard Filters")
 
 st.sidebar.divider()
-
 search_title = st.sidebar.text_input("🔍 Search Title",
                                      placeholder = "Search By Title...")
 
-st.divider()
+st.sidebar.divider()
 content_type = st.sidebar.selectbox(
     "Select Content Type",
     ["All", "Movie", "TV Show"]
 )
 
 st.sidebar.divider()
-
 country_list = (
     clean_df["country"]
     .str.split(",")
@@ -79,7 +65,6 @@ selected_country = st.sidebar.selectbox(
 )
 
 st.sidebar.divider()
-
 min_year = int(clean_df["release_year"].min())
 max_year = int(clean_df["release_year"].max())
 
@@ -89,19 +74,28 @@ selected_year = st.sidebar.slider("Select Release Year",
                                   value = (min_year,max_year))
 
 st.sidebar.divider()
-
 st.sidebar.subheader("Ratings")
 rating_list = sorted(clean_df["rating"].unique())
 
 selected_ratings = []
-for rating in rating_list:
-    if st.sidebar.checkbox(rating, value=True):
-        selected_ratings.append(rating)
+with st.sidebar.expander("Select Ratings", expanded=False):
+    col1, col2 = st.columns(2)
 
+    mid = (len(rating_list) + 1) // 2
+    left = rating_list[:mid]
+    right = rating_list[mid:]
 
-# -----------------------------
+    with col1:
+        for rating in left:
+            if st.checkbox(rating, value = True, key = f"left_{rating}"):
+                selected_ratings.append(rating)
+
+    with col2:
+        for rating in right:
+            if st.checkbox(rating, value = True, key = f"right_{rating}"):
+                selected_ratings.append(rating)
+
 # Apply Filters
-# -----------------------------
 filtered_df = clean_df.copy()
 
 if content_type != "All":
@@ -114,8 +108,7 @@ if selected_country != "All Countries":
         filtered_df["country"].str.contains(
             selected_country,
             case=False,
-            na=False
-        )
+            na=False)
     ]
 
 if selected_ratings:
@@ -125,13 +118,19 @@ if selected_ratings:
 else:
     filtered_df = filtered_df.iloc[0:0]
 
-# -----------------------------
+if search_title:
+    filtered_df = filtered_df[
+        filtered_df["title"].str.contains(
+            search_title,
+            case=False,
+            na=False
+        )
+    ]
+
 # KPI Metrics
-# -----------------------------
 total_titles = len(filtered_df)
 
 movies = (filtered_df["type"] == "Movie").sum()
-
 tv_shows = (filtered_df["type"] == "TV Show").sum()
 
 countries = (
@@ -139,21 +138,9 @@ countries = (
     .str.split(",")
     .explode()
     .str.strip()
-    .nunique()
-)
+    .nunique())
 
-if search_title:
-    filtered_df = filtered_df[filtered_df['title']
-                    .str.contains(search_title,
-                    case=False,
-                    na=False)
-    ]
-    st.write(f"Rows found: {len(filtered_df)}")
-    st.write(filtered_df[["title"]].head(20))
-
-# -----------------------------
 # KPI Cards
-# -----------------------------
 col1, col2, col3, col4 = st.columns(4)
 
 col1.metric("Total Titles", f"{total_titles:,}")
@@ -161,15 +148,11 @@ col2.metric("Movies", f"{movies:,}")
 col3.metric("TV Shows", f"{tv_shows:,}")
 col4.metric("Countries", f"{countries:,}")
 
-#------------------------------
 # Plotting
-#------------------------------
-
 st.divider()
-
 col1, col2 = st.columns(2)
 with col1:
-    st.subheader("Movies vs TV Shows")
+    st.subheader("Content Distribution")
 
     type_counts = filtered_df["type"].value_counts().reset_index()
     type_counts.columns = ["Content Type", "Count"]
@@ -179,7 +162,6 @@ with col1:
                  text = "Count",
                  color_discrete_sequence=["#E50914"])
     fig.update_layout(
-        height=500,
         showlegend=False,
         xaxis_title="Content Type",
         yaxis_title="Number of Titles"
@@ -191,12 +173,11 @@ with col1:
         fig,
         use_container_width=True,
         config={
-            "displayModeBar": False
-        }
+            "displayModeBar": False}
     )
 
 with (col2):
-    st.subheader("Titles Released Per Year")
+    st.subheader("Netflix Releases Over Time")
     yearly_titles = filtered_df.groupby('release_year'
                                         ).size().reset_index(name = "Count")
     fig = px.line(
@@ -207,7 +188,6 @@ with (col2):
     )
 
     fig.update_layout(
-        height=500,
         xaxis_title="Release Year",
         yaxis_title="Number of Titles"
     )
@@ -226,7 +206,6 @@ with (col2):
     )
 
 st.divider()
-
 col3, col4 = st.columns(2)
 
 with col3:
@@ -241,18 +220,15 @@ with col3:
                  text = "Titles",
                  color_discrete_sequence=["#E50914"])
     fig.update_layout(
-        height=500,
         yaxis = dict(categoryorder="total ascending",),
         showlegend=False,
         xaxis_title = "Titles",
-        yaxis_title = ""
-    )
+        yaxis_title = "")
     st.plotly_chart(
         fig,
         use_container_width=True,
         config={
-            "displayModeBar": False
-        }
+            "displayModeBar": False}
     )
 
 with col4:
@@ -268,25 +244,22 @@ with col4:
                  color_discrete_sequence=["#E50914"])
 
     fig.update_layout(
-        height=500,
         yaxis=dict(categoryorder="total ascending", ),
         showlegend=False,
         xaxis_title="Titles",
-        yaxis_title=""
-    )
+        yaxis_title="")
     st.plotly_chart(
         fig,
         use_container_width=True,
         config={
-            "displayModeBar": False
-        }
+            "displayModeBar": False}
     )
 
 st.divider()
 col5, col6 = st.columns(2)
 
 with col5:
-    st.subheader("Rating Distribution")
+    st.subheader("Content Rating Distribution")
     rating_counts = filtered_df['rating'].value_counts().reset_index()
 
     rating_counts.columns = ["Rating", "Titles"]
@@ -297,7 +270,6 @@ with col5:
                  color_discrete_sequence=["#E50914"])
 
     fig.update_layout(
-        height=500,
         showlegend=False,
         xaxis_title="Titles",
         yaxis_title=""
@@ -308,12 +280,11 @@ with col5:
         fig,
         use_container_width=True,
         config={
-            "displayModeBar": False
-        }
+            "displayModeBar": False}
     )
 
 with col6:
-    st.subheader("Movie Duration Distribution")
+    st.subheader("Movie Runtime Distribution")
 
     movie_df = filtered_df[filtered_df["type"] == "Movie"].copy()
 
@@ -323,33 +294,41 @@ with col6:
     fig = px.histogram(movie_df, x="duration", nbins=30, color_discrete_sequence=["#E50914"])
 
     fig.update_layout(
-        height=500,
         xaxis_title="Duration (Minutes)",
-        yaxis_title="Number of Movies"
-    )
+        yaxis_title="Number of Movies")
 
     st.plotly_chart(
         fig,
         use_container_width=True,
         config={
-            "displayModeBar": False
-        }
+            "displayModeBar": False}
     )
 
 st.divider()
+if search_title:
 
+    st.divider()
+
+    st.subheader("🔍 Search Results")
+
+    st.caption(f"Found {len(filtered_df)} matching titles")
+
+    st.dataframe(
+        filtered_df[
+            ["title", "type", "country", "release_year", "rating"]
+        ],
+        use_container_width=True
+    )
+
+st.divider()
 st.download_button(
     label = "📥 Download Filtered Dataset (CSV)",
     data = filtered_df.to_csv(index = False),
     file_name = "filtered_netflix_data.csv",
-    mime = "text/csv",
-)
+    mime = "text/csv")
 
-# -----------------------------
 # Dataset Preview
-# -----------------------------
 st.divider()
-
 st.subheader("📋 Filtered Dataset")
 
 st.caption(
@@ -357,7 +336,6 @@ st.caption(
 )
 
 st.divider()
-
 st.markdown(
     """
     <center>
